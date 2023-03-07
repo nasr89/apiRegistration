921bd300e1a1458039a127e3ce6fe8c4c812f9e4
nasr89 <nasrnasr8915@gmail.com>
2023-03-07 22:34:18 +0200

    third commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const User = require("../models/userModel");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
+const sendMail = require("../utils/email");
+const crypto = require("crypto");
 exports.signUp = async (req, res) => {
   try {
     //1- check if the email entered is valid
@@ -57,26 +59,92 @@ exports.login = async (req, res) => {
   }
 };
 
-exports.forgotPassword = async(req,res) => {
+exports.forgotPassword = async (req, res) => {
   try {
     // 1- check if the user withthe provided email exist
-    const user = await User.findOne({email: req.body.email});
-    if(!user){
-      return res.status(404).json({message: "the user with the provided email does not exist."});
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "the user with the provided email does not exist." });
     }
     // 2- create the reset token to be sended via email
     const resetToken = user.generatePasswordResetToken();
-    await user.save({ validateBeforeSave: false});
+    await user.save({ validateBeforeSave: false });
 
     // 3- send the token via email
     // http://127.0.0.1:3000/api/auth/resetPassword/dnksnsdfnhaydnaskjnasdn
     //3.1 : create this url
 
-    const url = `${req.protocol}://${req.get("host")}/api/auth/resetPassword/${resetToken}`;
+    const url = `${req.protocol}://${req.get(
+      "host"
+    )}/api/auth/resetPassword/${resetToken}`;
 
     const msg = `Forgot your password? Reset it by visiting the following link: ${url}`;
-    
+
+    // hon 3melne trycatch jdide b2alb haydik trycatch . leh? lsabab houwwe anno nahna hon ra7 neb3at l email fa yemkin hayda lemail ye5la2lna mechkil bass la2anno hon 3amna3mel await inside await fa mna3mella trycatch wa7da 7atta ma t2asser 3a chi lli akbar menna
+    try {
+      await sendMail({
+        email: user.email,
+        subject: "your password reset token: (valid for 10 min)",
+        message: msg,
+      });
+      res.status(200).json({
+        status: "success",
+        message: "the reset link was delivered to your email successful",
+      });
+    } catch (error) {
+      user.passwordResetToken = undefined;
+      user.passwordResetExpires = undefined;
+      await user.save({ validateBeforeSave: false });
+
+      res.status(500).json({
+        message:
+          "an error occured while sending the email, please try again in a moment",
+      });
+    }
   } catch (err) {
     console.log(err);
   }
-}
+};
+
+exports.resetPassword = async (req, res) => {
+  try {
+    const hashedToken = crypto
+      .createHash("sha256")
+      .update(req.params.token)
+      .digest("hex");
+
+    const user = await User.findOne({
+      passwordResetToken: hashedToken,
+      passwordResetExpires: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "the token is invalid, or expired. please request a new one",
+      });
+    }
+
+    if (req.body.password.length < 8) {
+      return res.status(400).json({ message: "password length is too short" });
+    }
+
+    if (req.body.password !== req.body.passwordConfirm) {
+      return res
+        .status(400)
+        .json({ message: "password & password Confirm are not the same " });
+    }
+
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    user.passwordChangeAt = Date.now();
+    await user.save();
+
+    return res.status(200).json({ message: "password changed successfully" });
+  } catch (err) {
+    console.log(err);
+  }
+};
--- utils/email.js
+++ utils/email.js
@@ -1,25 +1,24 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-exports.sendMail = async(options) => {
-    // 1- create the transporter(transporter hiyye metel machine bten2elna l email taba3na mnel lnodejs server la 3end luser ma7all ma houwwe mawjoud)
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    });
+exports.sendMail = async (options) => {
+  // 1- create the transporter(transporter hiyye metel machine bten2elna l email taba3na mnel lnodejs server la 3end luser ma7all ma houwwe mawjoud)
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
 
-    // 2- DEFINE the mail options:
-    const mailOptions = {
-        from: "Elie Hannouch <mentor@techlarious.com>",
-        to: options.email,
-        subject: options.subject,
-        text: options.message
-    }
+  // 2- DEFINE the mail options:
+  const mailOptions = {
+    from: "nasr nasr <nasrnasr8915@gmail.com>",
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
 
-    // 3-send the mail
-    await transporter.sendMail(mailOptions);
-
-}
+  // 3-send the mail
+  await transporter.sendMail(mailOptions);
+};
